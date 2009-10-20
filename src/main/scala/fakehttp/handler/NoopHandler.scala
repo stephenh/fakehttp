10e12229aec605e6100a269d55f8dbb0274c3bf0
Stephen Haberman <stephen@exigencecorp.com>
2009-10-20 16:54:27 -0500

    Handle ports in the host header.

--- src/main/scala/fakehttp/handler/NoopHandler.scala
+++ src/main/scala/fakehttp/handler/NoopHandler.scala
@@ -4,8 +4,12 @@ import org.jboss.netty.handler.codec.http._
 
 class NoopHandler extends HttpHandler {
   def handle(req: HttpRequest): HandleResult = {
-    val host = req.getHeader(HttpHeaders.Names.HOST)
-    val port = if (req.getMethod == HttpMethod.CONNECT) {
+    // Watch for foo.com:123
+    val parts = req.getHeader(HttpHeaders.Names.HOST).split(":")
+    val host = parts(0)
+    val port = if (parts.size > 1) {
+      parts(1).toInt
+    } else if (req.getMethod == HttpMethod.CONNECT) {
       req.getUri.split(":").last.toInt
     } else {
       80
