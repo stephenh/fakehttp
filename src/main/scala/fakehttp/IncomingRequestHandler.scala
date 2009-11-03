e2e821bdf08416d1bddae65f01569c4058384b0b
Stephen Haberman <stephen@exigencecorp.com>
2009-11-03 08:38:10 -0600

    Use wildcard imports.

--- src/main/scala/fakehttp/IncomingRequestHandler.scala
+++ src/main/scala/fakehttp/IncomingRequestHandler.scala
@@ -1,19 +1,8 @@
 package fakehttp
 
 import java.net.InetSocketAddress
-import org.jboss.netty.buffer.ChannelBuffer
-import org.jboss.netty.buffer.ChannelBuffers
-import org.jboss.netty.channel.Channel
-import org.jboss.netty.channel.Channels
-import org.jboss.netty.channel.ChannelFuture
-import org.jboss.netty.channel.ChannelFutureListener
-import org.jboss.netty.channel.ChannelHandlerContext
-import org.jboss.netty.channel.ChannelPipelineCoverage
-import org.jboss.netty.channel.ChannelStateEvent
-import org.jboss.netty.channel.ExceptionEvent
-import org.jboss.netty.channel.MessageEvent
-import org.jboss.netty.channel.DownstreamMessageEvent
-import org.jboss.netty.channel.SimpleChannelUpstreamHandler
+import org.jboss.netty.buffer._
+import org.jboss.netty.channel._
 import org.jboss.netty.channel.socket.ClientSocketChannelFactory
 import org.jboss.netty.handler.codec.http._
 import org.jboss.netty.handler.ssl.SslHandler
